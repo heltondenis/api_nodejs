041e264f1a6d16e8706b0d4b72bacf065afdf44f
Helton Denis <heltondeniss@gmail.com>
2018-07-27 21:52:02 -0300

    CRUD

--- index.js
+++ index.js
@@ -17,3 +17,48 @@ app.use('/', router);
 //inicia o servidor
 app.listen(port);
 console.log('API funcionando!');
+
+
+function execSQLQuery(sqlQry, res){
+  const connection = mysql.createConnection({
+  host     : 'localhost',
+  port     : 3306,
+  user     : 'root',
+  password : '',
+  database : 'agendamob'
+  });
+ 
+  connection.query(sqlQry, function(error, results, fields){
+      if(error) 
+        res.json(error);
+      else
+        res.json(results);
+      connection.end();
+      console.log('executou!');
+  });
+}
+
+// Visualizando todos
+router.get('/clientes', (req, res) =>{
+    execSQLQuery('SELECT * FROM Clientes', res);
+});
+
+
+// Visualizando com filtro
+router.get('/clientes/:id?', (req, res) =>{
+    let filter = '';
+    if(req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM Clientes' + filter, res);
+});
+
+// Deletando
+router.delete('/clientes/:id', (req, res) =>{
+    execSQLQuery('DELETE FROM Clientes WHERE ID=' + parseInt(req.params.id), res);
+});
+
+// Inserindo
+router.post('/clientes', (req, res) =>{
+    const nome = req.body.nome.substring(0,150);
+    const cpf = req.body.cpf.substring(0,11);
+    execSQLQuery(`INSERT INTO Clientes(Nome, CPF) VALUES('${nome}','${cpf}')`, res);
+});
